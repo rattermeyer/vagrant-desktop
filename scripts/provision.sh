91f8a9398cf5565af2e133c6fd2e352b9da69f7b
Richard Attermeyer <richard.attermeyer@gmail.com>
2015-11-07 06:38:14 +0100

    desktop updates

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
-cd /vagrant/data/ansible-dev-box
+if [ ! -d ansible-dev-desktop ]; then
+  git clone https://github.com/rattermeyer/ansible-dev-desktop.git
+  git checkout ubuntu-unity
+  cd ansible-dev-desktop
+else
+  cd ansible-dev-desktop
+  git pull
+fi
 ansible-playbook -i inventory dev.yml
--- scripts/update.sh
+++ scripts/update.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-apt-get update
+apt-get update 
 apt-get upgrade -y
-/etc/init.d/vboxadd setup
